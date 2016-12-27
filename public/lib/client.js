4fd84a00e658e8f2c35e8d06426c78f52915a79d
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2016-12-27 14:06:13 +0000

    Skip matched user & Rematch with another.
    The user can skip current user.
    The systems remembers skipped users.
    The system matches user with another user not skipped before.

--- public/lib/client.js
+++ public/lib/client.js
@@ -1,14 +1,75 @@
 var socket = io();
 
-$('#messageForm').submit(function () {
-    if ($('#message').val().trim() !== "") {
-        socket.emit('send message', $('#message').val());
-        $('#messages').append($('<li>').text($('#message').val()));
-        $('#message').val('');
-    }
+socket.on("receive message", function (msg) {
+    $("#messages").append($("<li>").addClass("partner").text(msg));
+});
+
+socket.on("matched", function () {
+    setUpChat();
+});
+
+socket.on("unmatched", function () {
+    $("#messages").empty();
+    $("#skipForm").hide();
+    $("#messageForm").submit(function () {
+        return false;
+    });
+
+    socket.emit("match", function (matched) {
+        if (matched) {
+            setUpChat();
+        } else {
+            $("#skipForm").hide();
+        }
+    });
+});
+
+$("#startForm").submit(function () {
+    socket.emit("match", function (matched) {
+        if (matched) {
+            setUpChat();
+        } else {
+            $("#skipForm").hide();
+        }
+    });
+
     return false;
 });
 
-socket.on('receive message', function (msg) {
-    $('#messages').append($('<li>').text(msg));
+$("#messageForm").submit(function () {
+    return false;
 });
+
+$("#skipForm").submit(function () {
+    return false;
+});
+
+var setUpChat = function () {
+    $("#messages").empty();
+
+    $("#messageForm").submit(function () {
+        if ($("#message").val().trim() !== "") {
+            socket.emit("send message", $("#message").val());
+            $("#messages").append($("<li>").addClass("user").text($("#message").val()));
+            $("#message").val('');
+        }
+        return false;
+    });
+
+    $("#startForm").hide();
+
+    $("#skipForm").show();
+
+    $("#skipForm").submit(function () {
+        socket.emit("skip", function (matched, user) {
+            console.log(user);
+            if (matched) {
+                setUpChat();
+            } else {
+                $("#messages").empty();
+                $("#skipForm").hide();
+            }
+        });
+        return false;
+    });
+};
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-var express = require('express'),
+var express = require("express"),
     app = express(),
-    http = require('http').Server(app),
-    io = require('socket.io')(http),
+    http = require("http").Server(app),
+    io = require("socket.io")(http),
     users = {};
 
-app.use(express.static(__dirname + '/public/lib'));
+app.use(express.static(__dirname + "/public/lib"));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/view/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/view/index.html");
 });
 
-io.on('connection', function (socket) {
+io.on("connection", function (socket) {
     //allocate a random username
     var i = 0, userExists = true;
     while (userExists) {
@@ -18,25 +18,20 @@ io.on('connection', function (socket) {
         if (users[newUsername] === undefined) {
             users[newUsername] = socket;
             socket.username = newUsername;
+            users[newUsername].skipped = [];
             userExists = false;
-
-            //allocate a random partner
-            for (var username in users) {
-                if (username !== socket.username && users[username]["partner"] === undefined) {
-                    users[username]["partner"] = socket.username;
-                    users[socket.username]["partner"] = username;
-                    break;
-                }
-            }
-        }
-        i++;
+        } else i++;
     }
 
-    socket.on('send message', function (msg) {
+    socket.on("match", function (cb) {
+        matchUsers(socket, cb);
+    });
+
+    socket.on("send message", function (msg) {
         if (msg.trim() !== "") {
             var partner = users[socket.username]["partner"];
             if (partner) {
-                users[partner].emit('receive message', msg);
+                users[partner].emit("receive message", msg);
             }
         }
     });
@@ -46,11 +41,40 @@ io.on('connection', function (socket) {
         var partnerUsername = users[socket.username]["partner"];
         if (partnerUsername) {
             delete users[partnerUsername]["partner"];
+            users[partnerUsername].emit("unmatched");
         }
         delete users[socket.username];
+    });
+
+    socket.on("skip", function (cb) {
+
+        var partnerUsername = users[socket.username]["partner"];
+        if (partnerUsername) {
+            delete users[partnerUsername]["partner"];
+            delete users[socket.username]["partner"];
+            users[socket.username].skipped.push(partnerUsername);
+            users[partnerUsername].emit("unmatched");
+        }
+
+        matchUsers(socket, cb);
     });
 });
 
 http.listen(3000, function () {
-    console.log('listening on *:3000');
+    console.log("listening on *:3000");
 });
+
+var matchUsers = function (socket, cb) {
+    var matched = false;
+    //allocate a random partner
+    for (var username in users) {
+        if (username !== socket.username && users[socket.username].skipped.indexOf(username) === -1 && users[username].skipped.indexOf(socket.username) === -1 && users[username]["partner"] === undefined) {
+            users[username]["partner"] = socket.username;
+            users[socket.username]["partner"] = username;
+            users[username].emit("matched");
+            matched = true;
+            break;
+        }
+    }
+    cb(matched, users[socket.username].skipped);
+};
