aec26f31a51d6ce94b653df3f1c6272035783871
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-02-28 16:42:48 +0000

    Fix minor error.

--- server/chat.js
+++ server/chat.js
@@ -56,6 +56,10 @@ module.exports = function (io) {
                                     break;
                                 }
                             }
+
+                            if (matched) {
+                                break;
+                            }
                         }
                     }
 
