d22c74ae68baab76052548261353843eb8b804ee
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2017-03-27 19:13:25 +0100

    Use a local SSL Certificate .

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require("express"),
-    app = express(),
-    http = require("http").Server(app),
-    io = require("socket.io")(http);
+const fs = require("fs"),
 
-http.listen(9000);
+    privateKey = fs.readFileSync("certs/server.key", "utf8"),
+    certificate = fs.readFileSync("certs/server.crt", "utf8"),
+    credentials = {key: privateKey, cert: certificate},
+
+    express = require("express"),
+    app = express(),
+    https = require("https").Server(credentials, app),
+    io = require("socket.io")(https);
+
+https.listen(9000);
 
 app.use(express.static(__dirname + "/public"));
 
